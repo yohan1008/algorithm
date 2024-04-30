3f7685fda877ba987d9ed1048bef920c2a7d7c84
DESKTOP-3LP8HEB\kangyohan <kangyohan754@google.com>
2024-04-30 12:48:45 +0900

    solved other O004

---
+++ other/O004/question/O004.java
@@ -0,0 +1,12 @@
+package other.O004.question;
+
+/**
+ * 문제 푼 횟수 : 1
+ * <p>
+ * 1005404400의 약수 중 10000 이상 99999 이하의 범위에 있는 것의 개수를 구해 주세요.
+ */
+
+public class O004 {
+    public static void main(String[] args) {
+    }
+}
