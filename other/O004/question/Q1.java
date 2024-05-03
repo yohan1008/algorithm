1bb0d5e8b15191a185c79f3ce5db847fc3061a65
DESKTOP-3LP8HEB\kangyohan <kangyohan754@google.com>
2024-05-03 17:10:45 +0900

    solved other O006

--- other/O004/question/Q1.java
+++ other/O004/question/Q1.java
@@ -1,12 +1,12 @@
 package other.O004.question;
 
 /**
- * 문제 푼 횟수 : 2
+ * 문제 푼 횟수 : 3
  * <p>
  * 1005404400의 약수 중 10000 이상 99999 이하의 범위에 있는 것의 개수를 구해 주세요.
  */
 
-public class O004 {
+public class Q1 {
     public static void main(String[] args) {
     }
 }
